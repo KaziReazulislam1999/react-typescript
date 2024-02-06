32dd8b26769a4f810bd599f154e20ef80c508f52
Kazi Reazul Islam <kazireazul1999@gmail.com>
2024-02-06 22:41:54 +0600

    typing useState Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import ButtonStyle from "./components/ButtonStyle";
-
-const btnStyle = {
-  backgroundColor: "red",
-  color: "#FFF",
-  padding: 20,
-};
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    return setCount(count + 1);
+  };
+  const handleDecrement = () => {
+    return setCount(count - 1);
+  };
+  const handleReset = () => {
+    return setCount(0);
+  };
   return (
     <div className="App">
-      <h2>Style Props</h2>
-      <ButtonStyle btnStyle={btnStyle} />
+      <h1>Counter App</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement} disabled={count === 0}>
+        Decrement
+      </button>
+      <button onClick={handleReset} disabled={count === 0}>
+        Reset
+      </button>
     </div>
   );
 }
